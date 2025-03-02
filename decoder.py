28ed76e4c4c97015f50e696d437fb0400eda724a
carlosrs14 <crincones578@gmail.com>
2025-03-01 19:01:06 -0500

    adding methods, relationships and interfaz functionality

--- decoder.py
+++ decoder.py
@@ -9,25 +9,48 @@ def json_to_plantuml(data:dict) -> str:
     plantuml_str += "@enduml"
     return plantuml_str
 
-def decodeClassDiagram(data:dict):
+def decodeClassDiagram(data:dict) -> str:
     visibilities = {
         "private": "-",
         "protected": "#",
         "package private": "~",
         "public": "+"
     }
+    relations_type = {
+        "inheritance": "<|--",
+        "composition": "*--",
+        "aggregation": "o--",
+        "association": "--",
+        "instantiation": "",
+        "realization": "",
+    }
+
     isStatic = '{static} '
     plantuml_str = ""
-    for clase in data["classes"]:
-        
+    # class
+    for clase in data.get("classes", []):
         # declare the class
-        plantuml_str += f"{'abstract ' if clase['isAbstract'] else ''}class {clase['name']}\n"
+        plantuml_str += f"{'abstract ' if clase.get('isAbstract', '') else ''}class {clase['name']}\n"
         
-        # adding attributes
-        for attribute in clase["attributes"]:
-            attribute_str = f"{clase['name']} : {visibilities[attribute['visibility']]}{isStatic if attribute['isStatic'] else ''}{attribute['name']} {attribute['type']}\n"
+        # adding attributes for clases
+        for attribute in clase.get("attributes", []):
+            attribute_str = f"{clase['name']} : {visibilities[attribute.get('visibility', '')]}{isStatic if attribute['isStatic'] else ''}{attribute['name']} {attribute['type']}\n"
             plantuml_str += attribute_str
 
+        # adding methods for clases    
+        for method in clase.get('methods', []):
+            plantuml_str += f"{clase.get('name', '')} : {method.get('returnType', '')} {method.get('name', '')}()\n"
+        
+        # adding relationsihps
+        for relation in clase.get("relationships", []):
+            relation_str = f"{clase['name']} {relations_type[relation.get('type', [])]} {relation['target']}\n"
+            plantuml_str += relation_str
+    
+    # interfaces
+    for interfaz in data.get("interfaces", []):
+        plantuml_str += f"interfaz {interfaz.get('name', '')}\n"
+        for method in interfaz.get('methods', []):
+            plantuml_str += f"{interfaz.get('name', '')} : {method.get('returnType', '')} {method.get('name', '')}()\n"
     return plantuml_str
 
 def decodeUseCaseDiagram(data:dict):
