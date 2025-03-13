15dcb1235bcc7605263569c77f637ecfd711d89d
Jonathan Enrrique Vizcaino Macias <jonayma0110@gmail.com>
2025-03-13 00:44:24 -0500

    volviendo el router como estaba

--- src/Routers.js
+++ src/Routers.js
@@ -1,8 +1,8 @@
 
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-//import Principal from './Vista/Principal'; // Asegúrate de que la ruta sea correcta
-import Principal from "./API/ai_api"
+import Principal from './Vista/Principal'; // Asegúrate de que la ruta sea correcta
+//import Principal from "./API/ai_api"
 const Routers = () => {
   return (
     <BrowserRouter>
