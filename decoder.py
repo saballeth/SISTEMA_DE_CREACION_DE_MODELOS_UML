cbfddce9905a347df07d7e5886c69a18ca80f576
Jonathan Enrrique Vizcaino Macias <jonayma0110@gmail.com>
2025-03-13 05:06:04 -0500

    corrección y acoplamiento de el parser para casos de uso

--- decoder.py
+++ decoder.py
@@ -65,6 +65,117 @@ def decodeClassDiagram(data:dict) -> str:
     
     return plantuml_str
 
-# to do
+
 def decodeUseCaseDiagram(data:dict):
-    return 'plantuml to generate'
+    plantuml_str = ""
+
+    # Actores
+    for actor in data.get("actors", []):
+        plantuml_str = decodeUseCaseActor(plantuml_str, actor)
+        
+    # Casos de uso globales
+    for use_case in data.get("useCases", []):
+        plantuml_str = decodeUseCase(plantuml_str, use_case)
+
+    # Paquetes
+    plantuml_str = decodeUseCasePackage(plantuml_str, data)
+
+    # Relaciones
+    for relation in data.get("relationships", []):
+        plantuml_str = decodeRelationships(plantuml_str, relation)     
+
+    return plantuml_str
+
+
+def decodeUseCaseActor(current_code:str, data) -> str:
+    plantuml_str = current_code
+
+    actor_name = data.get("name", "")
+    actor_alias = data.get("alias", "") 
+    actor_stereotype = f' <<{data["stereotype"]}>>' if "stereotype" in data else ""
+    actor_business = "/" if data.get("business", False) else ""
+
+    if actor_name:
+        plantuml_str += f"actor{actor_business} \"{actor_name}\""
+        if actor_alias:
+            plantuml_str += f" as {actor_alias}"
+        plantuml_str += actor_stereotype + "\n"
+    
+    return plantuml_str
+
+def decodeUseCase(current_code:str, data) -> str:
+    plantuml_str = current_code
+
+    usecase_name = data.get("name", "")
+    if usecase_name:
+        usecase_business = "/" if data.get("business", False) else ""
+        usecase_alias = data.get("alias", "")
+        usecase_stereotype = actor_stereotype = f' <<{data["stereotype"]}>>' if "stereotype" in data else ""
+        if usecase_alias:
+            plantuml_str += f'usecase{usecase_business} (\"{usecase_name}\") as {usecase_alias} {usecase_stereotype}\n'
+   
+    return plantuml_str
+
+
+def decodeUseCasePackage(current_code: str, data) -> str:
+    plantuml_str = current_code
+    packages = data.get("packages", [])
+    if not packages:
+        return plantuml_str
+    
+    for package in packages:
+        plantuml_str += f'package "{package["name"]}" as {package["alias"]} {{\n'
+
+        for use_case in package.get("useCases", []):
+            plantuml_str = decodeUseCase(plantuml_str, use_case) 
+
+        for actor in package.get("actors", []):
+            plantuml_str = decodeUseCaseActor(plantuml_str, actor)
+
+        plantuml_str = decodeUseCasePackage(plantuml_str, package)
+
+        plantuml_str += "}\n" 
+
+    return plantuml_str
+def decodeRelationships(current_code:str, data) -> str:
+    plantuml_str = current_code
+
+    relation_type = data.get("type", "")
+    relation_principal = data.get("principal", "")
+    relation_secondary = data.get("secondary", "")
+    relation_direction = data.get("direction", "")
+    relation_label = f":\"{data.get('label', '')}\"" if data.get('label') else ""
+
+    #Only actor_actor
+    relation_extend = data.get("extend", "")
+    
+    #only usecase_usecase 
+    relation_stereotype = data.get("stereotype", "")
+
+    if(relation_principal and relation_secondary):
+        if relation_type == "actor_actor": 
+            if relation_extend == ">":
+                plantuml_str += f"{relation_secondary} <|-- {relation_principal}"
+            elif relation_extend == "<":
+                plantuml_str += f"{relation_principal} <|-- {relation_secondary}"
+            else:
+                plantuml_str += f"{relation_principal} -{relation_direction}-> {relation_secondary}"
+            plantuml_str += relation_label + "\n"
+                
+        elif relation_type == "actor_usecase":
+            plantuml_str += f"{relation_principal} -{relation_direction}-> {relation_secondary}{relation_label}\n"
+
+        elif relation_type == "useCase_usecase":
+            if relation_stereotype == "include" :
+                plantuml_str += f"{relation_principal} .> {relation_secondary}: include\n"
+            elif relation_stereotype == "extends":
+                plantuml_str += f"{relation_principal} .> {relation_secondary}: extends\n"
+
+        elif relation_type == "package_package":
+            plantuml_str += f"{relation_principal} -{relation_direction}-> {relation_secondary}{relation_label}\n"
+
+    return plantuml_str
+
+
+        
+
--- main.py
+++ main.py
@@ -8,5 +8,5 @@ with open(sys.argv[1], "r", encoding="utf-8") as input:
 
 plant_uml_code = decoder.json_to_plantuml(data)
 
-with open("output.puml", "+w") as output:
+with open("output.puml", "+w", encoding="utf-8") as output:
     output.write(plant_uml_code)
